206b28dffab65e59a59557ec4cb8c6022f077fb7
James Dinsdale <hi@molovo.co>
2022-02-01 14:27:39 +0000

    Steal prismic configuration from Prismic theme in the hope that it works

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,1 +1,11 @@
-export const apiEndpoint = 'https://your-repo-name.cdn.prismic.io/api/v2'
+/**
+ * This file is used for setting up and connecting to Prismic.
+ */
+
+module.exports = {
+  // The domain name of your Prismic repository. This can be found in the URL of
+  // your repository.
+  //
+  // Example: 'my-repo' if your Prismic repository URL is 'my-repo.prismic.io'.
+  prismicRepo: 'your-repo-name',
+}
