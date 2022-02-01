a1c16f0a17508fc5e89ba83e03b8848c219dcc7e
James Dinsdale <hi@molovo.co>
2022-02-01 14:19:27 +0000

    Add homepage schema

--- src/types/pages/homepage.ts
+++ src/types/pages/homepage.ts
@@ -1,26 +1,33 @@
-import { Page, RichText } from '@superrb/gatsby-addons/types'
 import { graphql } from 'gatsby'
+import { Page } from '@superrb/gatsby-addons/types'
 
 interface Homepage extends Page {
-  data:
-    | Page['data']
-    | {
-        title: RichText
-        content: RichText
-      }
+  data: Page['data']
 }
 
 export const query = graphql`
   fragment Homepage on PrismicHomepage {
     _previewable
-    id
     uid
+    id
+    type
+    tags
     data {
-      title {
-        ...RichText
-      }
-      content {
-        ...RichText
+      meta_description
+      meta_navigation_title
+      meta_robots
+      meta_title
+      meta_image {
+        fluid {
+          base64
+          src
+        }
+        gatsbyImageData(
+          layout: CONSTRAINED
+          imgixParams: { q: 65 }
+          placeholder: BLURRED
+          breakpoints: [750, 1080, 1366, 1920, 2048, 2560, 3840, 4096, 5120]
+        )
       }
     }
   }
